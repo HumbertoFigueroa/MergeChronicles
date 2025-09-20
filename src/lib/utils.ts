cc2a2ec7ba5d322ca3389247d23449c928c81922
puber18 <pube_america01@hotmail.com>
2025-09-20 04:59:34 +0000

    te soy sincero, no se donde picarle para darte lo que necesitas

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function findImage(id: string, name?: string): string {
-  console.log(`findImage called with id: ${id}, name: ${name}`);
   const formattedName = name ? name.replace(/\s/g, '+') : id;
   return `https://placehold.co/200x200/FFFFFF/A149F8?text=${formattedName}&font=fredoka`;
 }
