52bf1af1518bc718825e4baea36cd8eb07ae87eb
puber18 <pube_america01@hotmail.com>
2025-09-22 01:14:35 +0000

    en que parte es el paso 6, no lo veo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzVb9oGdnKxMbtpECw-bC2k_9346h_Eoc",
+  apiKey: "AIzaSyCnFgtwCjJm5f5JZXYXeHzknErfMJTW73Y",
   authDomain: "studio-8739862538-2f294.firebaseapp.com",
   projectId: "studio-8739862538-2f294",
   storageBucket: "studio-8739862538-2f294.appspot.com",
